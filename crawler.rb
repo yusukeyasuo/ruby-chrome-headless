40723f9f31476617cd6c55608e0cf982e3666550
Yusuke Yasuo <yasuo.yusuke@redish.jp>
2019-03-03 21:32:19 +0900

    use headless mode output log

--- crawler.rb
+++ crawler.rb
@@ -7,25 +7,31 @@ require 'selenium-webdriver'
 ua = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36"
 
 # ブラウザ立ち上げモード
-caps = Selenium::WebDriver::Remote::Capabilities.chrome('chromeOptions' => {args: ["--user-agent=#{ua}", 'window-size=1280x800']})
+# caps = Selenium::WebDriver::Remote::Capabilities.chrome('chromeOptions' => {args: ["--user-agent=#{ua}", 'window-size=1280x800']})
 # ヘッドレスモード
-# caps = Selenium::WebDriver::Remote::Capabilities.chrome('chromeOptions' => {args: ["--headless","--no-sandbox", "--disable-setuid-sandbox", "--disable-gpu", "--user-agent=#{ua}", 'window-size=1280x800']})
+caps = Selenium::WebDriver::Remote::Capabilities.chrome('chromeOptions' => {args: ["--headless","--no-sandbox", "--disable-setuid-sandbox", "--disable-gpu", "--user-agent=#{ua}", 'window-size=1280x800']})
 driver = Selenium::WebDriver.for :chrome, desired_capabilities: caps
 
 #スクレイピングしたいサイト
+puts '===>start scraping'
 driver.navigate.to "https://www.jnto.go.jp/emergency/jpn/mi_guide.html"
+puts '===>navigate to https://www.jnto.go.jp'
 sleep 2
 
 driver.find_element(:class, 'filter-areas').click
+puts '===>find class filter-areas'
 sleep 1
 
 driver.find_element(:xpath, '//option[@value="a08_tokyo"]').click
+puts '===>find xpath option[@value="a08_tokyo"]'
 sleep 1
 
 driver.find_element(:id, 'searchBtn').click
+puts '===>find id searchBtn'
 sleep 5
 
 #Nokogiriを用いてHTMLをパースする
+puts '===>start nokogiri parse'
 doc = Nokogiri::HTML.parse(driver.page_source, nil, 'utf-8')
 area = doc.at_xpath('//div[@id="searchArea"]')
 nodes = area.xpath('//div[@class="element shuffle-item filtered"]')
@@ -34,3 +40,4 @@ puts nodes.count
 nodes.each do |node|
   puts node.at_xpath('section/h1').inner_html
 end
+puts '===>end scraping'
